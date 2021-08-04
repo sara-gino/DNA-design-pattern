37e49da485cc0c6c42b387c17e07fca88b1b2584
sara-gino <84020391+sara-gino@users.noreply.github.com>
2021-08-05 02:19:08 +0300

    Delete Find.py

--- Find.py
+++
@@ -1,9 +0,0 @@
-from DnaData import parser_seq, seq_to_find_or_count, get_index_sub_seq
-
-class Find:
-    def execute(self,command_arg):
-        dna_data=parser_seq(command_arg[0])
-        seq=dna_data[1]
-        seq_find=seq_to_find_or_count(command_arg[1])
-        ind=get_index_sub_seq(seq,seq_find)
-        print(ind)
